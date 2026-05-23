02172f6639d268580da27d540e1acd91b22e4907
M. Haroon <mharoon1578@gmail.com>
2026-05-23 11:08:45 +0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,3 @@
-
-if (typeof File === 'undefined') {
-  global.File = class File {
-    constructor(bits, name, options = {}) {
-      this.bits = bits;
-      this.name = name;
-      this.options = options;
-    }
-  };
-}
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Kodro Documentation',
