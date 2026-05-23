b03bf3582f422191a4035f04e261b29a5bebbfb4
M. Haroon <mharoon1578@gmail.com>
2026-05-23 10:57:16 +0500

    added sidebars.js

---
+++ sidebars.js
@@ -0,0 +1,20 @@
+/**
+ * Creating a sidebar enables you to:
+ - create an ordered group of docs
+ - render a sidebar for the docs of that plugin
+ - provide next/previous navigation
+
+ The sidebars can be generated from the filesystem, or explicitly defined here.
+
+ Create as many sidebars as you want.
+ */
+const sidebars = {
+  tutorialSidebar: [
+    'getting-started',
+    'pipeline',
+    'configuration',
+    // Add more documentation files here as they are created
+  ],
+};
+
+module.exports = sidebars;
